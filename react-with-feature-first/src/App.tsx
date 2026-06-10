0e8fbc83c54f5274e0305dd50a6ad72bb3c32f9b
AyrielNoriega <ingenierosenior.ecommerce@interrapidisimo.com>
2026-06-10 17:46:47 -0500

    first commit

---
+++ react-with-feature-first/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+    </>
+  );
+}
+
+export default App;
